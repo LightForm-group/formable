1d6388c458a13cc4724cd44e0badb65fdc826b67
aplowman <adam.jp@live.co.uk>
2020-04-21 17:13:17 +0100

    Fix tests. Fixes #3.

--- tests/test_load_response.py
+++ tests/test_load_response.py
@@ -25,7 +25,8 @@ class LoadResponseTestCase(unittest.TestCase):
         self.assertTrue(resp.is_uniaxial())
 
     def test_get_yield_stress_value_idx(self):
-        'Check specifying `value_idx` in get_yield_stress works as expected.'
+        """Check specifying `value_idx` in `LoadResponse.get_yield_stress` works as
+        expected."""
 
         num_increments = 11
         equiv_strain = np.linspace(0, 1e-3, num=num_increments)
@@ -33,22 +34,22 @@ class LoadResponseTestCase(unittest.TestCase):
 
         resp = LoadResponse(true_stress=stress, equivalent_strain=equiv_strain)
         vals = [0.00035, 0.00045]
-        yield_point_criteria = YieldPointCriteria(
-            threshold='equivalent_strain',
-            values=vals,
-        )
+        yield_point_criteria = YieldPointCriteria('equivalent_strain', vals)
 
         yield_stress_all_vals = resp.get_yield_stress(yield_point_criteria)
         yield_stress_by_vals = [
             resp.get_yield_stress(yield_point_criteria, value_idx=i) for i in range(2)
         ]
 
-        # Combine into dicts to compare:
-        all_vals_dict = {k: v for v, k in zip(*yield_stress_all_vals)}
-        by_vals_dict = {k[0]: v[0] for v, k in yield_stress_by_vals}
+        # Combine into dicts to compare (keyed by YPC value index):
+        all_vals_dict = {ypc_val_idx: stress
+                         for stress, ypc_val_idx in zip(*yield_stress_all_vals)}
 
-        for val_idx, yld_stress in all_vals_dict.items():
-            self.assertTrue(np.allclose(yld_stress, by_vals_dict[val_idx]))
+        by_vals_dict = {ypc_val_idx[0]: stress
+                        for stress, ypc_val_idx in yield_stress_by_vals}
+
+        for ypc_val_idx, stress in all_vals_dict.items():
+            self.assertTrue(np.allclose(stress, by_vals_dict[ypc_val_idx]))
 
 
 class LoadResponseSetTestCase(unittest.TestCase):
@@ -70,19 +71,27 @@ class LoadResponseSetTestCase(unittest.TestCase):
         yield_point_criteria = YieldPointCriteria('equivalent_strain', 2e-4)
         load_set.calculate_yield_stresses(yield_point_criteria)
 
-        # Index into the first yield point criteria and the first yield point value:
-        yld_stresses_all = load_set.yield_stresses
-        yld_stresses_all_arr = np.array(yld_stresses_all[0][0]['values'])
+        ypc_idx = 0
+        ypc_val_idx = 0
+        resp_idx = 3
 
-        yld_stresses_i = [i.get_yield_stress(yield_point_criteria) for i in responses]
-        yld_stresses_i_arr = np.array([i[0][0] for i in yld_stresses_i])
+        for i in load_set.yield_stresses:
+            if i['YPC_idx'] == ypc_idx and i['YPC_value_idx'] == ypc_val_idx:
+                all_resp_idx = np.array(load_set.yield_stresses[ypc_idx]['response_idx'])
+                value_idx = np.where(all_resp_idx == resp_idx)[0][0]
+                yld_stress_all = load_set.yield_stresses[ypc_idx]['values'][value_idx]
+                break
 
-        self.assertTrue(
-            np.allclose(yld_stresses_all_arr, yld_stresses_i_arr)
-        )
+        yld_stress_resp = responses[resp_idx].get_yield_stress(
+            yield_point_criteria,
+            value_idx=ypc_val_idx,
+        )[0]
+
+        self.assertTrue(np.allclose(yld_stress_all, yld_stress_resp))
 
     def test_set_single_yield_stress_consistent_insufficient_yield(self):
-        '`test_set_single_yield_stress_consistent`, but including unachieved yield points'
+        """Check yield stresses of set are consistent with those from individual
+        responses, including responses with unachieved yield points."""
 
         num_increments = 11
         num_responses = 5
@@ -103,16 +112,23 @@ class LoadResponseSetTestCase(unittest.TestCase):
         yield_point_criteria = YieldPointCriteria('equivalent_strain', 2e-4)
         load_set.calculate_yield_stresses(yield_point_criteria)
 
-        # Index into the first yield point criteria and the first yield point value:
-        yld_stresses_all = load_set.yield_stresses
-        yld_stresses_all_arr = np.array(yld_stresses_all[0][0]['values'])
+        ypc_idx = 0
+        ypc_val_idx = 0
+        resp_idx = 3
 
-        yld_stresses_i = [i.get_yield_stress(yield_point_criteria) for i in responses]
-        yld_stresses_i_arr = np.array([i[0][0] for i in yld_stresses_i if i[0].size])
+        for i in load_set.yield_stresses:
+            if i['YPC_idx'] == ypc_idx and i['YPC_value_idx'] == ypc_val_idx:
+                all_resp_idx = np.array(load_set.yield_stresses[ypc_idx]['response_idx'])
+                value_idx = np.where(all_resp_idx == resp_idx)[0][0]
+                yld_stress_all = load_set.yield_stresses[ypc_idx]['values'][value_idx]
+                break
 
-        self.assertTrue(
-            np.allclose(yld_stresses_all_arr, yld_stresses_i_arr)
-        )
+        yld_stress_resp = responses[resp_idx].get_yield_stress(
+            yield_point_criteria,
+            value_idx=ypc_val_idx,
+        )[0]
+
+        self.assertTrue(np.allclose(yld_stress_all, yld_stress_resp))
 
     def test_raise_on_distinct_incremental_data(self):
         'Check LoadResponseSet raises when passed response objects with distinct data.'
@@ -142,39 +158,3 @@ class LoadResponseSetTestCase(unittest.TestCase):
         response_2 = LoadResponse(true_stress=stress_2, equivalent_strain=equiv_strain_2)
 
         LoadResponseSet([response_1, response_2])
-
-    def test_yield_stress_ordering(self):
-        'Check yield stress ordering with respect to yield point criteria.'
-
-        num_increments = 11
-        num_responses = 5
-        equiv_strain = np.linspace(0, 1e-3, num=num_increments)
-        stress = np.random.random((num_responses, num_increments, 3, 3))
-
-        responses = [
-            LoadResponse(true_stress=stress[idx], equivalent_strain=equiv_strain)
-            for idx in range(num_responses)
-        ]
-        load_set = LoadResponseSet(responses)
-
-        yield_point_criteria_1 = YieldPointCriteria('equivalent_strain', [1e-4, 2e-4])
-        yield_point_criteria_2 = YieldPointCriteria('equivalent_strain', [3e-4])
-
-        load_set.calculate_yield_stresses(yield_point_criteria_1)
-        load_set.calculate_yield_stresses(yield_point_criteria_2)
-
-        # Basic checks:
-        self.assertTrue(len(load_set.yield_stresses) == 2)      # One for each YPC
-        self.assertTrue(len(load_set.yield_stresses[0]) == 2)   # One for each YPC value
-        self.assertTrue(len(load_set.yield_stresses[1]) == 1)   # One for each YPC value
-
-        # Check yield stresses for a given YPC/value:
-        test_yld_stress_all = load_set.yield_stresses[0][1]['values']
-        yld_stresses_i = np.array([
-            i.get_yield_stress(yield_point_criteria_1, value_idx=1)[0][0]
-            for i in responses
-        ])
-
-        self.assertTrue(
-            np.allclose(test_yld_stress_all, yld_stresses_i)
-        )
